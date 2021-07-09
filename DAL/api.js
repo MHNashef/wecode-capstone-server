b0f8f0b42e9be6773b5feb35e3c24526a0d0eae4
Hisham Nashef <hishamnshf@gmail.com>
2021-07-09 21:24:23 +0300

    added routing to database for client-side recipe page

--- DAL/api.js
+++ DAL/api.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -6,9 +6,9 @@ const connection = mysql.createConnection({
   password: "9y_+Y8wV5?",
   database: "recipe_book",
 });
-connection.connect(err => {
-    if (err) throw err;
-})
+connection.connect((err) => {
+  if (err) throw err;
+});
 
 function getRecipes(res, orderBy, limit, where) {
   let myQuery = "SELECT * FROM recipes";
@@ -22,10 +22,41 @@ function getRecipes(res, orderBy, limit, where) {
 }
 
 function getRecipeById(res, recipeId) {
-    connection.query(`SELECT * FROM recipes WHERE id = ${recipeId}`, (err, result) => {
-        if (err) throw err;
-        res.send(result);
-    })
+  connection.query(
+    `SELECT * FROM recipes WHERE id = ${recipeId}`,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
 }
 
-module.exports = {getRecipes, getRecipeById};
+function getRecipeInstructions(res, recipeId) {
+  connection.query(
+    `SELECT * FROM instructions WHERE recipe_id = ${recipeId} ORDER BY step_number ASC`,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+}
+
+function getRecipeIngredients(res, recipeId) {
+  connection.query(
+    `SELECT amount, measurement_name, ingredient_name FROM recipe_ingredients 
+    JOIN measurements ON measurement_id = measurements.id
+    JOIN ingredients ON ingredient_id = ingredients.id
+    WHERE recipe_id = ${recipeId}`,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+}
+
+module.exports = {
+  getRecipes,
+  getRecipeById,
+  getRecipeInstructions,
+  getRecipeIngredients,
+};
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,27 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { getRecipes, getRecipeById } = require("../DAL/api");
+const {
+  getRecipes,
+  getRecipeById,
+  getRecipeInstructions,
+  getRecipeIngredients,
+} = require("../DAL/api");
 
-
-router.get('/', function(req, res, next) {
-    getRecipes(res);
+router.get("/", function (req, res) {
+  getRecipes(res);
 });
 
-router.get('/:limit', function(req, res, next) {
+router.get("/:limit", function (req, res) {
   getRecipes(res, null, req.params.limit);
 });
 
-router.get('/recent/:limit', function(req, res, next) {
-  getRecipes(res, 'date_created', req.params.limit);
+router.get("/recent/:limit", function (req, res) {
+  getRecipes(res, "date_created", req.params.limit);
 });
 
-router.get('/popular/:limit', function(req, res, next) {
-  getRecipes(res, 'views', req.params.limit);
+router.get("/popular/:limit", function (req, res) {
+  getRecipes(res, "views", req.params.limit);
 });
 
-router.get('/recipeId/:id', function(req, res) {
+router.get("/recipeId/:id", function (req, res) {
   getRecipeById(res, req.params.id);
-})
+});
 
+router.get("/recipeId/:id/instructions", function (req, res) {
+  getRecipeInstructions(res, req.params.id);
+});
+
+router.get("/recipeId/:id/ingredients", function (req, res) {
+  getRecipeIngredients(res, req.params.id);
+});
 
 module.exports = router;
