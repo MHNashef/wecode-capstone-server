248ad9862ffd15651b536d24693a60c80f55bbb4
Hisham Nashef <hishamnshf@gmail.com>
2021-07-21 02:53:08 +0300

    added routing and queries for pagination

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const {getSearchRes} = require("../DAL/api");
+const { getSearchRes, getSearchCount } = require("../DAL/api");
 
 router.get("/:searchStr", function (req, res) {
-    getSearchRes(res, req.params.searchStr);
+  getSearchRes(res, req.params.searchStr);
+});
+
+router.get("/:searchStr/count", (req, res) => {
+  getSearchCount(res, req.params.searchStr);
+});
+
+router.get("/:searchStr/l/:limit/p/:page", function (req, res) {
+  getSearchRes(res, req.params.searchStr, null, req.params.limit, req.params.page);
 });
 
 module.exports = router;
